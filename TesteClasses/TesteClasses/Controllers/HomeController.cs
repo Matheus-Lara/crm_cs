dbb793ca45ff15d6f89f6f99c961bca8ef877ec0
Matheus Oliveira de Lara <matheusodelara@gmail.com>
2021-08-28 18:35:51 -0300

    cria rota home para não retornar 404

---
+++ TesteClasses/TesteClasses/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TesteClasses.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("bem vindo à api do CRM");
+        }
+    }
+}
